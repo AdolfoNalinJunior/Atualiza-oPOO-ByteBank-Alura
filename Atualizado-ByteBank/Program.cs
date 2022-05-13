ede18f8421a561c5503d61a57ba2e80ed1f849d8
AdolfoNalinJunior <adolfonalin22@outlook.com>
2022-05-13 20:50:20 -0300

    POO 2022-Alura-Atualização

---
+++ Atualizado-ByteBank/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Boa Vindas, ao seu banco, ByteBank!");
+
+string titular = "André Bessa";
+string conta = "10123-x";
+int numero_agencia = 23;
+string nome_agencia = "Agencia Central";
+double saldo = 93.50;
+
+Console.WriteLine("Titular: " + titular);
+Console.WriteLine("Conta: " + conta);
+Console.WriteLine("Número Agencia: " + nome_agencia);
+Console.WriteLine("Nome da Agencia: " + nome_agencia);
+Console.WriteLine("Saldo: " + saldo);
