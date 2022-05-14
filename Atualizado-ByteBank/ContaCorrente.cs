4bf4855fedcfa3da710890917c368283f0da57de
AdolfoNalinJunior <adolfonalin22@outlook.com>
2022-05-14 14:34:25 -0300

    Primeira classe: mapeamento da classe ContaCorrente

---
+++ Atualizado-ByteBank/ContaCorrente.cs
@@ -0,0 +1,12 @@
+﻿namespace Atualizado_ByteBank
+{
+    public class ContaCorrente
+    {
+    public string titular;
+    public string conta;
+    public string nome_agencia;
+    public int numero_agencia;
+    public double saldo;
+    }
+
+}
